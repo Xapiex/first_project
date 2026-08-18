9e6790f65b86ea42b8511b491b7aa1ba983d1265
xapiex <sapie151@gmail.com>
2026-08-17 21:17:36 -0400

    Initial commit: Cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Establecer plantilla cookie cutter',
+    author='Grupo 7',
+    license='MIT',
+)
